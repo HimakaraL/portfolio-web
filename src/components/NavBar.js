e17d1beb79d326188441987c8a5ae0a59be88f39
HimakaraL <himakaral@outlook.com>
2025-05-07 14:55:08 +0530

    Initial commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,55 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { useState, useEffect } from "react";
+
+export const NavBar = () => {
+  const [activeLink, setActiveLink] = useState("home");
+   const [scrolled, setScrolled] = useState(false);
+
+   useEffect(() => {
+    const onScroll = () => {
+        if (window.scrollY > 50) {
+            setScrolled(true);
+        }
+        else {
+            setScrolled(false);
+        }
+    }
+
+    
+   })
+
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">
+          <img src="" alt="logo"></img>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav">
+          <span className="navbar-toggler-icon"></span>
+        </Navbar.Toggle>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#skills">Skills</Nav.Link>
+            <Nav.Link href="#projects">Projects</Nav.Link>
+          </Nav>
+          <span className="navbar-text">
+            <div className="social-icon">
+              <a href="#">
+                <i className="fa-brands fa-github"></i>
+              </a>
+              <a href="#">
+                <i className="fa-brands fa-linkedin"></i>
+              </a>
+              <a href="#">
+                <i className="fa-brands fa-twitter"></i>
+              </a>
+            </div>
+          </span>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
